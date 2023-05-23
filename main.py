ce32286b91c87184e4ff83a832f9d0dccd399c65
Aleksei Lobachev <90100576+LexLobachev@users.noreply.github.com>
2023-05-23 09:41:37 +0400

    add fire shot

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ def draw(canvas):
     for coroutine in range(100):
         coroutine = blink(
             canvas,
-            random.randint(1, y-2),
-            random.randint(1, x-2),
+            random.randint(1, y - 2),
+            random.randint(1, x - 2),
             random.randint(1, 20),
             random.choice('+*.:')
         )
         coroutines.append(coroutine)
+
+    fire_shot = fire(canvas, y - 2, x / 2)
+    coroutines.append(fire_shot)
 
     while True:
         for coroutine in coroutines.copy():
@@ -49,6 +52,36 @@ async def blink(canvas, row, column, blink_timing, symbol='*'):
         canvas.addstr(row, column, symbol)
         for _ in range(blink_timing):
             await asyncio.sleep(0)
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
 
 
 if __name__ == '__main__':
